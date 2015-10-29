6e689239a19bb0c68698db2a33ec978809d5fdca
Vincent <vincent@server.com>
2015-10-29 12:58:43 -0400

    fixes for tgt aligned data

--- trialbytrial.m
+++ trialbytrial.m
@@ -22,17 +22,28 @@ neur=mat2cell(([profiles{sunitid_revidx,2}])',ones(110,1));
 
 %% new code
 neur=neur(~cellfun('isempty',gsdata.allsacdelay));
-neur(:,2)=cellfun(@(x) x(1).rast, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),'UniformOutput',false); %NSS trials rast:
-neur(:,3)=cellfun(@(x) x(1).alignt, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),'UniformOutput',false); %NSS trials alignt:
-neur(:,4)=cellfun(@(x) x(1).evttime, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),'UniformOutput',false); %NSS trials event time:
-%reminder of events: 'cue' 'eyemvt' 'fix' 'rew' 'fail'
-behav=cellfun(@(x) x(1).trialnb, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),'UniformOutput',false); %NSS trials:
-behav(:,2)=cellfun(@(x) [x(2:end).trialnb], gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),'UniformOutput',false);%all SS trials
-behav(:,3)=cellfun(@(x) x.nsst, gsdata.allsacdelay(~cellfun('isempty',gsdata.allsacdelay),1),'UniformOutput',false);%NSS RT
-behav(:,4)=cellfun(@(x) x.ncst, gsdata.allsacdelay(~cellfun('isempty',gsdata.allsacdelay),1),'UniformOutput',false);%NCS RT
-behav(:,5)=cellfun(@(x) x(1).eyevel, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay),1),'UniformOutput',false);%eye velocity
+neur(:,2,1)=cellfun(@(x) x(1).rast, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),...
+    'UniformOutput',false); %NSS trials rast, aligned to sac
+neur(:,2,2)=cellfun(@(x) x(1).rast, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 2),...
+    'UniformOutput',false); %NSS trials rast, aligned to tgt
+neur(:,3,1)=cellfun(@(x) x(1).alignt, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),...
+    'UniformOutput',false); %NSS trials alignt
+neur(:,4,1)=cellfun(@(x) x(1).evttime, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),...
+    'UniformOutput',false); %NSS trials event time. Reminder of events: 'cue' 'eyemvt' 'fix' 'rew' 'fail'
+neur(:,4,2)=cellfun(@(x) x(1).evttime, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 2),...
+    'UniformOutput',false); %NSS trials event time. Reminder of events: 'cue' 'eyemvt' 'fix' 'rew' 'fail'
+behav=cellfun(@(x) x(1).trialnb, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay), 1),...
+    'UniformOutput',false); %NSS trials:
+behav(:,2)=cellfun(@(x) [x(2:end).trialnb], gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay),...
+    1),'UniformOutput',false);%all SS trials
+behav(:,3)=cellfun(@(x) x.nsst, gsdata.allsacdelay(~cellfun('isempty',gsdata.allsacdelay),1),...
+    'UniformOutput',false);%NSS RT
+behav(:,4)=cellfun(@(x) x.ncst, gsdata.allsacdelay(~cellfun('isempty',gsdata.allsacdelay),1),...
+    'UniformOutput',false);%NCS RT
+behav(:,5)=cellfun(@(x) x(1).eyevel, gsdata.allndata(~cellfun('isempty',gsdata.allsacdelay),2),...
+    'UniformOutput',false);%eye velocity
 
-neur=neur(~cellfun('isempty',behav(:,2)),:);%removing bad apples
+neur=neur(~cellfun('isempty',behav(:,2)),:,:);%removing bad apples
 behav=behav(~cellfun('isempty',behav(:,2)),:);%removing bad apples
 
 %% behavioral analysis
@@ -105,7 +116,7 @@ legend('boxoff')
 %% current method
 % keep cluster 1 only for the moment
 behav=behav([neur{:,1}]==101,:);
-neur=neur([neur{:,1}]==101,:);
+neur=neur([neur{:,1}]==101,:,:);
 % get slopes (and if really daring, get curve's start as well)
 % figure; hold on;
 for rec=1:size(neur,1)
@@ -113,10 +124,10 @@ for rec=1:size(neur,1)
     for trial=1:size(neur{rec,2},1)
         %         figure('Name',['Trial ' num2str(trial)]); hold on;
         % conv % 200 to 500 ms before saccades
-        tr_wd_conv = gauss_filtconv(neur{rec,2}(trial,neur{rec,3}-500:neur{rec,3}-1),50);
+        tr_wd_conv = gauss_filtconv(neur{rec,2,1}(trial,neur{rec,3,1}-500:neur{rec,3,1}-1),50);
         %         plot(conv);
         wd_shift=500-find(tr_wd_conv==max(tr_wd_conv));% plot(tr_wd_conv(1:end-wd_shift));
-        neur{rec,5}(trial)={glmfit(1:500-wd_shift,tr_wd_conv(1:end-wd_shift))};
+        neur{rec,5,1}(trial)={glmfit(1:500-wd_shift,tr_wd_conv(1:end-wd_shift))};
         %         yfit = glmval(coeff,1:301-wd_shift,'identity');
         
         %         pf = polyfit(1:301, neur{cell,2}(trial,neur{cell,3}-500:neur{cell,3}-200),4);
@@ -130,7 +141,7 @@ end
 % figure; histogram(cellfun(@(x) x(2)*1000,neur(:,5)),10)
 %reminder: neur 'labels' are 'cluster id','NSST raster','NSST alignment','NSST evt time','slopes'
 
-[ccoefs,sigcc]=cellfun(@(x,y) corrcoef(x(2,:),y),cellfun(@(x) [x{:}], neur(:,5),'UniformOutput',false),behav(:,3),'UniformOutput',false);
+[ccoefs,sigcc]=cellfun(@(x,y) corrcoef(x(2,:),y),cellfun(@(x) [x{:}], neur(:,5,1),'UniformOutput',false),behav(:,3),'UniformOutput',false);
 ccoefs=[ccoefs{:}];ccoefs=ccoefs(2,1:2:end);
 sigcc=[sigcc{:}];sigcc=sigcc(2,1:2:end);
 sigcoefs=sigcc<0.05; %significant correlation coefficients between pre-sac slope and RT
@@ -139,18 +150,18 @@ sigma=20;
 for rec=1:size(neur,1)
     %     neur{sigcoefs(cell),6}=conv_raster(neur{sigcoefs(cell),2}(:,neur{sigcoefs(cell),3}-1900:neur{sigcoefs(cell),3}),20);
     %     figure;plot(neur{sigcoefs(sigramp),6});
-    [neur{rec,6},~,neur{rec,7}]=conv_raster(neur{rec,2}(:,neur{rec,3}-1900:neur{rec,3}+3*sigma),sigma);
+    [neur{rec,6,1},~,neur{rec,7,1}]=conv_raster(neur{rec,2,1}(:,neur{rec,3,1}-1900:neur{rec,3,1}+3*sigma),sigma);
     % no need for sem actually
 end
 fh(2)=figure('Name','ramp_RT correlation - Mean pre-sac activity');hold on;
 cmap = colormap(gcf);
-sigslope_RT=nanmean(cat(1,neur{sigcoefs,6})); sigslope_RT_sem=std(cat(1,neur{sigcoefs,6}))/ sqrt(size(cat(1,neur{sigcoefs,6}),1));
+sigslope_RT=nanmean(cat(1,neur{sigcoefs,6,1})); sigslope_RT_sem=std(cat(1,neur{sigcoefs,6,1}))/ sqrt(size(cat(1,neur{sigcoefs,6,1}),1));
 %plot confidence intervals
 patch([1:length(sigslope_RT),fliplr(1:length(sigslope_RT))],[sigslope_RT-sigslope_RT_sem,fliplr(sigslope_RT+sigslope_RT_sem)],cmap(1,:),'EdgeColor','none','FaceAlpha',0.1);
 %plot sdf
 ploth(1)=plot(sigslope_RT,'Color',cmap(1,:),'LineWidth',2,'DisplayName','significant RT/slope correlation'); 
 
-nsigslope_RT=nanmean(cat(1,neur{sigcc>=0.05,6})); nsigslope_RT_sem=std(cat(1,neur{sigcc>=0.05,6}))/ sqrt(size(cat(1,neur{sigcc>=0.05,6}),1));
+nsigslope_RT=nanmean(cat(1,neur{sigcc>=0.05,6,1})); nsigslope_RT_sem=std(cat(1,neur{sigcc>=0.05,6,1}))/ sqrt(size(cat(1,neur{sigcc>=0.05,6,1}),1));
 %plot confidence intervals
 patch([1:length(nsigslope_RT),fliplr(1:length(nsigslope_RT))],[nsigslope_RT-nsigslope_RT_sem,fliplr(nsigslope_RT+nsigslope_RT_sem)],cmap(20,:),'EdgeColor','none','FaceAlpha',0.1);
 %plot sdf
